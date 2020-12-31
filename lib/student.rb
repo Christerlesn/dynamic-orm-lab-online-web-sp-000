b7ae1d721d23b625e4dde0bb84e40bcaffcce775
Christerle Nounoute <christerlesn@gmail.com>
2020-12-31 22:20:03 +0000

    Automatically backed up by Learn

--- lib/student.rb
+++ lib/student.rb
@@ -4,4 +4,9 @@ require 'interactive_record.rb'
 
 class Student < InteractiveRecord
 
+  def initialize(attributes={})
+    attributes.each do |attr_name, attr_value|
+      self.send("#{attr_name}=", attr_value)
+    end
+  end
 end
