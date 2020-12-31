c119b85f67e989091f0358e3f900b940bd5c4f7a
Christerle Nounoute <christerlesn@gmail.com>
2020-12-31 21:44:20 +0000

    Automatically backed up by Learn

--- lib/student.rb
+++ lib/student.rb
@@ -3,8 +3,17 @@ require 'active_support/inflector'
 require 'interactive_record.rb'
 
 class Student < InteractiveRecord
+  ATTRIBUTES = {
+    :id => INTEGER PRIMARY KEY,
+    :name => TEXT,
+    :grade => TEXT
+  }
   def self.table_name
     "#{self.to_s.downcase.pluralize}"
+  end
+
+  def self.column_names(attributes_array)
+    ATTRIBUTES.each {|attribute_name| puts "attribute_name"}
   end
 
 end
