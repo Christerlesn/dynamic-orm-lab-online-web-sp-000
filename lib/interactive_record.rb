dc3fe68b45531dc5b404fcaecb8fc60271616878
Christerle Nounoute <christerlesn@gmail.com>
2020-12-31 22:17:19 +0000

    Automatically backed up by Learn

--- lib/interactive_record.rb
+++ lib/interactive_record.rb
@@ -2,7 +2,6 @@ require_relative "../config/environment.rb"
 require 'active_support/inflector'
 
 class InteractiveRecord
-  module ClassMethods
       def table_name
         "#{self.to_s.downcase.pluralize}"
       end
